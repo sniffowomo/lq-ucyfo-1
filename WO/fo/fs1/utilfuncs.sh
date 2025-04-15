0808112399e4ace688bf082f12a38815996285dc
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-15 11:33:56 +0000

    🫦

--- WO/fo/fs1/utilfuncs.sh
+++ WO/fo/fs1/utilfuncs.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
-# This bash srcript is for installing the KL docker image here
+# UTility Functions Mentioned in the course
+
 clear
 
 # Colors
@@ -14,48 +15,18 @@ export NC='\033[0m' # No Color
 
 # Commands
 
-hea1() {
+h1() {
     echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
     echo -e "${PURPLE}$1${NC}"
     echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
 }
 
-b1() {
-    hea1 "UV Jupyter Lan Installation Commands"
-}
+# Functions
 
-c1() {
-    # Get name of Project
-    echo -e "Enter the name of the project: "
-    read name_of_project
-    if [ -z "$name_of_project" ]; then
-        echo -e "${RED}BASTARD ! Project name cannot be empty${NC}"
-        exit 1
-    fi
-
-    # Commands to execute
-    CO1="uv init $name_of_project"
-    CO2="cd $name_of_project"
-    CO3="uv add --dev ipykernel"
-    CO4="uv tree"
-
-    # Show Commands
-    echo -e ""
-    echo -e "${GREEN}---Commands to execute:---"
-    echo -e ">$CO1"
-    echo -e ">$CO2"
-    echo -e ">$CO3"
-    echo -e ">$CO4"
-    echo -e "Executing....${NC}"
-
-    # Execution Commands
-    eval "$CO1"
-    eval "$CO2"
-    eval "$CO3"
-    eval "$CO4"
-
+# Cast function to convert hex to decimal
+ca1() {
+    h1 "cast convert hexadecimal to function"
+    TO_CONVERT=$1
 }
 
 # Execution
-b1
-c1
