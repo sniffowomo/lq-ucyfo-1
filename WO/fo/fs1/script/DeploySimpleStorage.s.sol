2b004181d24a39c6a4f9468a675f13c3d47f073d
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-15 10:36:23 +0000

    🫦

--- WO/fo/fs1/script/DeploySimpleStorage.s.sol
+++ WO/fo/fs1/script/DeploySimpleStorage.s.sol
@@ -9,6 +9,7 @@ import {SimpleStorage} from "../src/SimpleStorage.sol";
 contract DeploySimpleStorage is Script {
     function run() external returns (SimpleStorage) {
         
+        // Foundry CheatCode - https://book.getfoundry.sh/cheatcodes/
         vm.startBroadcast();
 
         SimpleStorage simpleStorage = new SimpleStorage();
