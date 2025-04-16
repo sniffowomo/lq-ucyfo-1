68e82c0d41a9c9f464a69798cc0ce64c3203ed5b
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-16 11:58:13 +0000

    🫦

--- WO/fo/ffs2/script/Counter.s.sol
+++ WO/fo/ffs2/script/Counter.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {BootySniff} from "../src/Counter.sol";
 
 contract pantySmell is Script {
     Counter public counter;
