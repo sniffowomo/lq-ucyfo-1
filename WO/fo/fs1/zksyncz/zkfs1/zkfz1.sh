7772131ff1156843372d972b243f14320842b0f1
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-16 06:34:17 +0000

    🫦

---
+++ WO/fo/fs1/zksyncz/zkfs1/zkfz1.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+# UTility Functions Mentioned in the course
+
+clear
+
+# Colors
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export WHITE='\033[0;37m'
+export NC='\033[0m' # No Color
+
+# Commands
+
+h1() {
+    echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
+    echo -e "${PURPLE}$1${NC}"
+    echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
+}
+
+# Installaz
+
+install_zksync() {
+    h1 "Install Zksync"
+    CO1="curl -L https://raw.githubusercontent.com/matter-labs/foundry-zksync/main/install-foundry-zksync | bash"
+    echo -e "${GREEN}Command: ${NC}${CO1}"
+    eval "$CO1"
+}
+
+# building for zksync
+build_zk() {
+    h1 "Building Forge Zksync Project"
+    CO1="forge build --zksync -vvvvv"
+    echo -e "${GREEN}Command: ${NC}${CO1}"
+    eval "$CO1"
+}
+
+# Execution
+build_zk
