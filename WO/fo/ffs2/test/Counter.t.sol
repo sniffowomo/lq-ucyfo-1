b9047fa5cfc4abf5fb342f16ed3e29b5d7bfbe8f
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-16 12:03:17 +0000

    🫦

--- WO/fo/ffs2/test/Counter.t.sol
+++ WO/fo/ffs2/test/Counter.t.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {BootySniff} from "../src/Counter.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
+    BootySniff public bootysniff;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+    bootysniff = new BootySniff();
+    bootysniff.setNumber(0);
     }
 
     function test_Increment() public {
