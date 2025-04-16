0018b3cc905af42b73bfd11ef80daea4b3c57c0f
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-16 11:27:46 +0000

    🫦

--- WO/fo/ffs2/src/Counter.sol
+++ WO/fo/ffs2/src/Counter.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import {BootyScat} from "./Booty.sol";
 
-contract Counter {
+contract BootySniff {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
