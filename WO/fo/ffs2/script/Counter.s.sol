33a88b81bf7ed31509b0a8dc2d6d4cb03e577728
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-16 12:15:07 +0000

    🫦

--- WO/fo/ffs2/script/Counter.s.sol
+++ WO/fo/ffs2/script/Counter.s.sol
@@ -1,18 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console} from "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
 import {BootySniff} from "../src/Counter.sol";
 
-contract BootySniffScript is Script {
-    BootySniff public booty;
-
-    function setUp() public {}
-
+contract Deploy is Script {
     function run() public {
         vm.startBroadcast();
 
-        booty = new BootySniff();
+        BootySniff booty = new BootySniff();
 
         vm.stopBroadcast();
     }
