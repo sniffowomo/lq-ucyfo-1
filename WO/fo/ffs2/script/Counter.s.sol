c486547cb1ef3971ddfb57770aada286f7110d83
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-16 12:20:11 +0000

    🫦

--- WO/fo/ffs2/script/Counter.s.sol
+++ WO/fo/ffs2/script/Counter.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Script} from "forge-std/Script.sol";
-import {BootySniff} from "../src/Counter.sol";
+import {BootySniff} from "../src/BootySniff.sol";
 
 contract Deploy is Script {
     function run() public {
