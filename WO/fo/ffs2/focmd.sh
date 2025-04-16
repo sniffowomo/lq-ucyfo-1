d5b809e6dcec0929185991dafd8843c0c9d4fff9
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-16 13:02:29 +0000

    🫦

--- WO/fo/ffs2/focmd.sh
+++ WO/fo/ffs2/focmd.sh
@@ -93,7 +93,7 @@ fo_test() {
 ###############################################
 # Forge Create
 ################################################
-CONTRACT_PATH_CREATE="src/Counter.sol:BootySniff"
+CONTRACT_PATH_CREATE="src/BootySniff.sol:BootyToken"
 
 fo_create_holeksy() {
     hea1 "Foundry Create - One of contract deployment"
