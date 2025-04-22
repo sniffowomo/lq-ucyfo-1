02659233804d7233828e043e26b5a7665aabbd76
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-22 14:46:17 +0000

    🫦

--- WO/fo/s1fsu1/src/Counter.sol
+++ WO/fo/s1fsu1/src/Counter.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {Booty} from "./Booty.sol";
+
 contract PantyPiss {
     uint256 public number;
 
@@ -10,5 +12,10 @@ contract PantyPiss {
 
     function increment() public {
         number++;
+    }
+
+    // Function that just shows a message not even related
+    function viewMessage() public pure returns (string memory) {
+        return "Drink Woman Piss";
     }
 }
