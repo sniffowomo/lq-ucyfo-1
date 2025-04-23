a7f5ae93959e4b35320efb6eefb8062dacdc3a8c
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-23 13:01:08 +0000

    🫦

--- WO/fo/ffs2/script/multirape.s.sol
+++ WO/fo/ffs2/script/multirape.s.sol
@@ -5,11 +5,10 @@ import "forge-std/Script.sol";
 import "../src/FundMe.sol";
 
 /**
-The script below is deploying multipl contracts 
-- Since you dont need t use the variable for each contract
-- Using `new` keyword , The names of the contract are accroding to what is in the actual file 
+ * The script below is deploying multipl contracts 
+ * - Since you dont need t use the variable for each contract
+ * - Using `new` keyword , The names of the contract are accroding to what is in the actual file
  */
-
 contract RapeAll is Script {
     address priceFeedAddress = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
 
