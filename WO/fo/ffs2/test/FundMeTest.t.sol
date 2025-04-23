fac6d880b01bdaf5fd18e3e593a62618b0d7f115
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-23 02:52:57 +0000

    🫦

--- WO/fo/ffs2/test/FundMeTest.t.sol
+++ WO/fo/ffs2/test/FundMeTest.t.sol
@@ -5,7 +5,13 @@ pragma solidity ^0.8.18;
 import {Test} from "forge-std/Test.sol";
 
 contract FundMeTest is Test {
-    function setUp() external {}
+    uint256 number  = 1;
+    
+    function setUp() external {
+        number = 2;
+    }
 
-    function testDemo() public{}
+    function testDemo() public{
+        assertEq(number, 2);
+    }
 }
