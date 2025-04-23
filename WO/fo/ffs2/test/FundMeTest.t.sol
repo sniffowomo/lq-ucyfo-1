016a59bf42df67c9de23eb9e58e11bfd6d9dfd0e
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-23 03:50:28 +0000

    🫦

--- WO/fo/ffs2/test/FundMeTest.t.sol
+++ WO/fo/ffs2/test/FundMeTest.t.sol
@@ -10,7 +10,7 @@ contract FundMeTest is Test {
 
     // Setup used for deployment of the contract
     function setUp() external {
-        FundMe fundMe = new FundMe();
+        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
     }
 
     function testMinDollarIsFive() public {
