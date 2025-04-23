2b51fbb5314858a3c75a0926174daaaf09e90176
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-23 03:52:10 +0000

    🫦

--- WO/fo/ffs2/test/FundMeTest.t.sol
+++ WO/fo/ffs2/test/FundMeTest.t.sol
@@ -7,10 +7,11 @@ import {FundMe} from "../src/FundMe.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+    address priceFeedAddress = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
 
     // Setup used for deployment of the contract
     function setUp() external {
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(priceFeedAddress);
     }
 
     function testMinDollarIsFive() public {
