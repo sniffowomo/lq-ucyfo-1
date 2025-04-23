7fa33a4dad5171fcc42ae87fe5a9fccb37b8021e
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-23 02:58:02 +0000

    🫦

--- WO/fo/ffs2/test/FundMeTest.t.sol
+++ WO/fo/ffs2/test/FundMeTest.t.sol
@@ -12,7 +12,7 @@ contract FundMeTest is Test {
     }
 
     function testDemo() public{
-        
+        console.log("Duck all nite !!!");
         console.log("Number: ", number);
         assertEq(number, 2);
     }
