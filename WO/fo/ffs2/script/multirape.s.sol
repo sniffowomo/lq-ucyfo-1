9524039fd7145b5f8589054e63459b5c94778e2e
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-23 12:44:12 +0000

    🫦

--- WO/fo/ffs2/script/multirape.s.sol
+++ WO/fo/ffs2/script/multirape.s.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
-import "../src/Fundme.sol";
+import "../src/FundMe.sol";
 
 contract RapeAll is Script {
     function run() public {
         vm.startBroadcast();
 
-        Fundme fundMe = new Fundme();
-        BootySmell booty = new BootySmell();
+        FundmeContract fundMe = new Fundme();
+        BootySmellContract booty = new BootySmell();
         
         vm.stopBroadcast();
     }
