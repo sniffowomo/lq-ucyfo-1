447366eb9a82b1104d2aeafc40f207dae2a33635
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-23 12:47:35 +0000

    🫦

--- WO/fo/ffs2/script/multirape.s.sol
+++ WO/fo/ffs2/script/multirape.s.sol
@@ -5,10 +5,11 @@ import "forge-std/Script.sol";
 import "../src/FundMe.sol";
 
 contract RapeAll is Script {
+    address priceFeedAddress = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
     function run() public {
         vm.startBroadcast();
 
-        FundMe fundMe = new FundMe();
+        FundMe fundMe = new FundMe(priceFeedAddress);
         BootySmell booty = new BootySmell();
         
         vm.stopBroadcast();
