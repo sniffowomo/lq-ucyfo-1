820dc146cfdc8aa7bcd9061964b869cb4e944872
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-23 02:20:13 +0000

    🫦

--- WO/fo/ffs2/src/PriceConverter.sol
+++ WO/fo/ffs2/src/PriceConverter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import {AggregatorV3Interface} from "@chainlink/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "chainlink/AggregatorV3Interface.sol";
 
 library PriceConverter {
     function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
