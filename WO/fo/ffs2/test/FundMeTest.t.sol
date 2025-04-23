43bf1b00f3cd052683556931ad535f8129d83dcf
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-23 04:29:24 +0000

    🫦

--- WO/fo/ffs2/test/FundMeTest.t.sol
+++ WO/fo/ffs2/test/FundMeTest.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {Booty} from "../src/Booty.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
