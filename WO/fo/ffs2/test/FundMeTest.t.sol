6506cf14cd3bdaeba3bf050650f42e9b3c2bd612
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-23 03:23:25 +0000

    🫦

--- WO/fo/ffs2/test/FundMeTest.t.sol
+++ WO/fo/ffs2/test/FundMeTest.t.sol
@@ -6,10 +6,14 @@ import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract FundMeTest is Test {
+    FundMe fundMe;
+
     // Setup used for deployment of the contract
     function setUp() external {
         FundMe fundMe = new FundMe();
     }
 
-    function testMinDollarIsFive() public {}
+    function testMinDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18, "Minimum dollar amount is not 5");
+    }
 }
