e4b22cfef59ddc2f620f296cf7eb650a000b2f85
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-24 18:39:34 +0000

    🫦

--- WO/tezt/t2/cmdrun/0xa3AE6cDcF57E08437B17Ce5C8E53232cc4C876Fc.sol
+++ WO/tezt/t2/cmdrun/0xa3AE6cDcF57E08437B17Ce5C8E53232cc4C876Fc.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.4;
 
 interface BootyToken {
+
     event Approval(address indexed owner, address indexed spender, uint256 value);
     event Transfer(address indexed from, address indexed to, uint256 value);
 
