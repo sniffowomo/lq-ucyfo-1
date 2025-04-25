d854f59bf16dfd3cddb8de84b3b0be6b7c67714b
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-04-25 13:22:34 +0000

    🫦

--- WO/fo/rev1/focmd.sh
+++ WO/fo/rev1/focmd.sh
@@ -115,7 +115,7 @@ fo_create_holeksy() {
         --private-key ${keyz[0]} \
         --etherscan-api-key ${ETHERSCAN_API_KEY} \
         --optimize true \
-        --optimizer-runs 999\
+        --optimizer-runs 9999 \
         --build-info --build-info-path outz/ \
         --verify --broadcast \
         --out outz/ \
